f8748fac5f4b97d18e9320b1b8d8a88d29fba9b4
Cristopher David Chingo Rivera <166523123+DavidCh02@users.noreply.github.com>
2024-09-11 02:32:15 -0500

    Update forms.py

--- trivia_game/forms.py
+++ trivia_game/forms.py
@@ -29,4 +29,4 @@ class AnswerForm(forms.Form):
     def __init__(self, *args, **kwargs):
         question = kwargs.pop('question')
         super().__init__(*args, **kwargs)
-        self.fields['selected answer'].choices = [(answer, answer) for answer in question.get_answers()]
+        self.fields['selected_answer'].choices = [(answer, answer) for answer in question.get_answers()]

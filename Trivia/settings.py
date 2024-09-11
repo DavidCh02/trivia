b47b2b1ce256f56850ac47db78ab062f7e5e02de
Cristopher David Chingo Rivera <166523123+DavidCh02@users.noreply.github.com>
2024-09-10 23:08:56 -0500

    Update settings.py

--- Trivia/settings.py
+++ Trivia/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a6e80d44260d9ae09adecedfb2f58dd6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['trivia-85mh.onrender.com', 'localhost', '127.0.0.1']
 
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
